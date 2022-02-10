154538e2f587f53282048f5aa19f77806444781f
Godamunna D.R.B.N.B <99312319+It21213380@users.noreply.github.com>
2022-02-10 05:37:31 +0000

    completed 01

--- Tute01.c
+++ Tute01.c
@@ -6,6 +6,25 @@
 
 int main() {
   
+  //Declaring variables
+  float num1,num2,total=0,avg=0;
+
+//Getting marks from user
+  printf("Enter your subject 1 mark: \n");
+  scanf("%f",&num1);
+
+  printf("Enter your subject 2 mark: \n");
+  scanf("%f",&num2);
+
+//calculating the total
+  total=num1+num2;
+
+//calculating the average
+  avg=total/2;
+
+
+  printf("Your average mark is: %2.f",avg);
+
   return 0;
 }
 
